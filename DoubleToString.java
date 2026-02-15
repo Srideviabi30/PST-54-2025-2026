c94a45b04d3981002de77c7aab0dc790f975d447
Sridevi V <srideviabi30@gmail.com>
2026-02-15 10:00:54 +0530

    Add files via upload

---
+++ DoubleToString.java
@@ -0,0 +1,12 @@
+package Patterns;
+
+public class DoubleToString {
+
+	public static void main(String[] args) {
+		Double value=56.3;
+				String str=Double.toString(value);
+				System.out.println("Integer"+str);
+
+	}
+
+}
---
+++ IntegerToString.java
@@ -0,0 +1,13 @@
+package Patterns;
+
+public class IntegerToString {
+
+	public static void main(String[] args) {
+		int number=236;
+
+        
+        String str1 = String.valueOf(number);
+        System.out.println("Integer()"+ str1);
+	}
+        	
+}
